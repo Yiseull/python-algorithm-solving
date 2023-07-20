382144f04e175fcf8f1cec81c7be8fd1fc652bc1
Yiseul Park <omjl5123@catholic.ac.kr>
2023-07-21 02:41:31 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 9.89 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,9 +1,8 @@
 def solution(n):
     n_binary_one_count = bin(n).count('1')
-    number = n + 1
     while 1:
-        if n_binary_one_count == bin(number).count('1'):
+        n += 1
+        if n_binary_one_count == bin(n).count('1'):
             break
-        number += 1
             
-    return number
+    return n
