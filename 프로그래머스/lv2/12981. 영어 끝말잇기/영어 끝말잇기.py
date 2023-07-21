f1f89d5ddc284670a0994c937c216880c235737f
Yiseul Park <omjl5123@catholic.ac.kr>
2023-07-21 14:00:21 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,10 +1,10 @@
 def solution(n, words):
-    word_set = set()
-
-    for i, word in enumerate(words):
-        if (i > 0 and words[i - 1][-1] != word[0]) or word in word_set or len(word) == 1:
+    word_set = set([words[0]])
+    
+    for i in range(1, len(words)):
+        if words[i - 1][-1] != words[i][0] or words[i] in word_set:
             return [i % n + 1, i // n + 1]
 
-        word_set.add(word)
+        word_set.add(words[i])
 
     return [0, 0]
