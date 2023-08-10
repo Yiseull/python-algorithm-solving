ddb0f58586a9eef7644e546a6e6067e8bf98ec97
Yiseul Park <omjl5123@catholic.ac.kr>
2023-08-10 10:46:28 +0900

    [level 2] Title: 택배상자, Time: 382.77 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv2/131704. 택배상자/택배상자.py
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.py
@@ -2,12 +2,9 @@ def solution(order):
     answer = 0
     stack = []
     cur, i = 1, 0
+    
     while cur < len(order) + 1:
-        if cur == order[i]:
-            answer += 1
-            i += 1
-        else:
-            stack.append(cur)
+        stack.append(cur)
         cur += 1
         
         while (stack and stack[-1] == order[i]):
