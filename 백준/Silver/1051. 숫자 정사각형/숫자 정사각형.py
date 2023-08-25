ec935eaacbb3921f5f6c09e3481762a39c56befc
Yiseul Park <omjl5123@catholic.ac.kr>
2023-08-25 15:14:19 +0900

    [Silver III] Title: 숫자 정사각형, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
@@ -3,14 +3,11 @@ input = sys.stdin.readline
 
 
 def solution(n, m, rectangle) -> int:
-    answer = 1
-    for i in range(n):
-        for j in range(m):
-            for extra_len in range(1, min(n - i, m - j)):
+    for extra_len in range(min(n, m) - 1, -1, -1):
+        for i in range(n - extra_len):
+            for j in range(m - extra_len):
                 if rectangle[i][j] == rectangle[i + extra_len][j] == rectangle[i][j + extra_len] == rectangle[i + extra_len][j + extra_len]:
-                    answer = max(answer, pow(extra_len + 1, 2))
-
-    return answer
+                    return (extra_len + 1) ** 2
 
 
 if __name__ == '__main__':
