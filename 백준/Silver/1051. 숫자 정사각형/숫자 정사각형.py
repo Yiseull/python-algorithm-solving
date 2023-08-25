f9dab68b42f63fa2055f94fe3f9abb75a34defb4
Yiseul Park <omjl5123@catholic.ac.kr>
2023-08-25 14:56:38 +0900

    [Silver III] Title: 숫자 정사각형, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
@@ -2,22 +2,13 @@ import sys
 input = sys.stdin.readline
 
 
-def max_square(i, j, n, m, rectangle) -> int:
-    max_len = 1
-    extra_len = 0
-    while i + extra_len < n and j + extra_len < m:
-        if rectangle[i][j] == rectangle[i + extra_len][j] == rectangle[i][j + extra_len] == rectangle[i + extra_len][j + extra_len]:
-            max_len = max(max_len, extra_len + 1)
-        extra_len += 1
-
-    return max_len * max_len
-
-
 def solution(n, m, rectangle) -> int:
     answer = 1
     for i in range(n):
         for j in range(m):
-            answer = max(answer, max_square(i, j, n, m, rectangle))
+            for extra_len in range(1, min(n - i, m - j)):
+                if rectangle[i][j] == rectangle[i + extra_len][j] == rectangle[i][j + extra_len] == rectangle[i + extra_len][j + extra_len]:
+                    answer = max(answer, pow(extra_len + 1, 2))
 
     return answer
 
@@ -25,5 +16,5 @@ def solution(n, m, rectangle) -> int:
 if __name__ == '__main__':
     n, m = map(int, input().split())
     rectangle = [input().rstrip() for _ in range(n)]
-    
+
     print(solution(n, m, rectangle))
