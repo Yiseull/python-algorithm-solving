4b0badae893b9d9cca901c046c7d076cbc90ee6d
Yiseul Park <omjl5123@catholic.ac.kr>
2023-09-15 10:54:05 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 452 ms, Memory: 56768 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+
+def main() -> None:
+    n, m = map(int, input().split())
+    array = [input().strip() for _ in range(n)]
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    max_length = 0
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if array[i - 1][j - 1] == '1':
+                dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]) + 1
+    max_length = max(map(max, dp))
+
+    print(max_length ** 2)
+
+
+main()
