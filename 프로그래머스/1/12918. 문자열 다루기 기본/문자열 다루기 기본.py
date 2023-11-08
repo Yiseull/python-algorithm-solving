31d5038fba5e551bcd33c11b07334e85a4e3211c
Yiseul Park <omjl5123@catholic.ac.kr>
2023-11-08 15:09:30 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,6 +1,2 @@
 def solution(s):
-    for c in s:
-        if c.isalpha():
-            return False
-        
-    return len(s) == 4 or len(s) == 6
+    return s.isdigit() and (len(s) == 4 or len(s) == 6)
