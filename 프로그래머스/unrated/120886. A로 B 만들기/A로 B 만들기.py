ecc35031b3601df9ceaa4c552b488e9d1530877b
Yiseul Park <omjl5123@catholic.ac.kr>
2023-12-15 07:17:27 +0900

    [level unrated] Title: A로 B 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,4 +1,2 @@
-from collections import Counter
-
 def solution(before, after) -> int:
-    return 1 if Counter(before) == Counter(after) else 0
+    return 1 if sorted(before) == sorted(after) else 0
