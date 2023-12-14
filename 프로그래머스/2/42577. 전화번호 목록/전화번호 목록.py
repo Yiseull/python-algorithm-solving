0944da29acc476d2855d305a330a8ce08f681105
Yiseul Park <omjl5123@catholic.ac.kr>
2023-12-14 20:06:07 +0900

    [level 2] Title: 전화번호 목록, Time: 88.52 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -2,7 +2,7 @@ def solution(phone_book) -> bool:
     phone_book.sort()
     
     for phone1, phone2 in zip(phone_book, phone_book[1:]):
-        if phone1 == phone2[:len(phone1)]:
+        if phone2.startswith(phone1):
             return False
     
     return True
