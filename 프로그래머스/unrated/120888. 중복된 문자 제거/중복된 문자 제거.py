f347df3b2658a887b057659670f94ed7f6dcfbc6
Yiseul Park <omjl5123@catholic.ac.kr>
2023-12-15 07:13:18 +0900

    [level unrated] Title: 중복된 문자 제거, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -1,9 +1,2 @@
-from collections import defaultdict
-
 def solution(my_string):
-    my_dict = defaultdict(int)
-    
-    for s in my_string:
-        my_dict[s] += 1
-        
-    return ''.join(my_dict.keys())
+    return ''.join(dict.fromkeys(my_string))
