da453f0cda1faa493ba1a9235c6883f5b7ab0064
Yiseul Park <omjl5123@catholic.ac.kr>
2023-12-18 10:34:53 +0900

    Time: 211 ms (72.57%), Space: 17.9 MB (52.55%) - LeetHub

---
+++ LeetCode/0704-binary-search/0704-binary-search.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)
+        while left < right:
+            mid = (left + right) // 2
+            
+            if nums[mid] > target:
+                right = mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                return mid
+        
+        return -1
