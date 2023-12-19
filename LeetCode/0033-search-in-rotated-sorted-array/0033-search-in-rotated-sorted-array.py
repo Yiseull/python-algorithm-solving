ba1e51bdd40093c286ade9f3cb1f875d0e2b2e84
Yiseul Park <omjl5123@catholic.ac.kr>
2023-12-19 15:38:05 +0900

    Time: 53 ms (19.99%), Space: 16.6 MB (85.40%) - LeetHub

---
+++ LeetCode/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,29 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # 최솟값을 구해서 피벗으로 설정
+        left: int = 0
+        right: int = len(nums) - 1
+        while left < right:
+            mid: int = (left + right) // 2
+                
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+                
+        pivot: int = left
+            
+        # 피벗 기준 이진 검색
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid: int = (left + right) // 2
+            mid_privot: int = (mid + pivot) % len(nums)
+            
+            if nums[mid_privot] < target:
+                left = mid + 1
+            elif nums[mid_privot] > target:
+                right = mid - 1
+            else:
+                return mid_privot
+        
+        return -1
