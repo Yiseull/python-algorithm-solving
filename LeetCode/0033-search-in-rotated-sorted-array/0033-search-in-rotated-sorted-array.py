a3de7e35e17063d2dc69b707ad418a89f41fefe6
Yiseul Park <omjl5123@catholic.ac.kr>
2023-12-19 15:41:45 +0900

    Time: 53 ms (19.99%), Space: 16.6 MB (58.58%) - LeetHub

--- LeetCode/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ LeetCode/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,29 +1,19 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # 최솟값을 구해서 피벗으로 설정
-        left: int = 0
-        right: int = len(nums) - 1
-        while left < right:
-            mid: int = (left + right) // 2
-                
-            if nums[mid] > nums[right]:
-                left = mid + 1
-            else:
-                right = mid
-                
-        pivot: int = left
+        pivot:int = nums.index(min(nums))
             
         # 피벗 기준 이진 검색
         left, right = 0, len(nums) - 1
         while left <= right:
             mid: int = (left + right) // 2
-            mid_privot: int = (mid + pivot) % len(nums)
+            mid_pivot: int = (mid + pivot) % len(nums)
             
-            if nums[mid_privot] < target:
+            if nums[mid_pivot] < target:
                 left = mid + 1
-            elif nums[mid_privot] > target:
+            elif nums[mid_pivot] > target:
                 right = mid - 1
             else:
-                return mid_privot
+                return mid_pivot
         
         return -1
