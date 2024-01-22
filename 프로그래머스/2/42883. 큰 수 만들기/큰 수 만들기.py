4fc0f1f7631237084a7369a365af1ceb7c4e772a
Yiseul Park <omjl5123@catholic.ac.kr>
2024-01-23 08:10:00 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,13 @@
+'''
+1. 숫자들을 스택에 넣는다.
+2. 스택의 마지막 숫자와 현재 숫자를 비교해서 스택 숫자가 클 떄까지 스택에서 꺼내고 현재 숫자를 넣는다.
+'''
+def solution(number: str, k: int) -> str:
+    stack = []
+    for i, n in enumerate(number):
+        while stack and stack[-1] < n and k > 0:
+            stack.pop()
+            k -= 1
+        stack.append(n)
+        
+    return ''.join(stack[:-k]) if k else ''.join(stack)
