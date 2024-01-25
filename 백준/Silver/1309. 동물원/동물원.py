5cfb906f9f1094e4d5da0b6b64993538b79d8588
Yiseul Park <omjl5123@catholic.ac.kr>
2024-01-25 21:59:05 +0900

    [Silver I] Title: 동물원, Time: 620 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -3,11 +3,11 @@ input = sys.stdin.readline
 
 
 def solution(n: int) -> int:
-        a, b, c = 1, 1, 1
+        a, b = 1, 1
         for i in range(n - 1):
-            a, b, c = a + b + c, a + c, a + b
+            a, b = a + b + b, a + b
 
-        return (a + b + c) % 9901
+        return (a + b + b) % 9901
 
 
 if __name__ == '__main__':
