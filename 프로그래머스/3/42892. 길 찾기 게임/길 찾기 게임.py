3aa3c6fe3f5a69851c6ade004a44770de277a7dd
박이슬 <omjl5123@catholic.ac.kr>
2024-02-23 22:51:45 +0900

    [level 3] Title: 길 찾기 게임, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -3,21 +3,21 @@ sys.setrecursionlimit(10000)
 
 
 def solution(nodeinfo: list) -> list:
-    nodeinfo = sorted([node + [i + 1] for i, node in enumerate(nodeinfo)], key=lambda x: x[0])
     answer = [[], []]
+    nodeinfo = sorted([node + [i + 1] for i, node in enumerate(nodeinfo)])
     
-    def order(nodeinfo: list) -> list:
+    def order(nodeinfo: list):
         if nodeinfo:
             root = 0
             for i, node in enumerate(nodeinfo):
-                if node[1] > nodeinfo[root][1]:
+                if nodeinfo[root][1] < node[1]:
                     root = i
-                    
+
             answer[0].append(nodeinfo[root][2])
             order(nodeinfo[:root])
             order(nodeinfo[root+1:])
             answer[1].append(nodeinfo[root][2])
-    
+        
     order(nodeinfo)
-    
+        
     return answer
