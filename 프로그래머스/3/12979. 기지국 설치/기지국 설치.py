d9854dc40fe17b6bf80e76b0c4f325553b213871
박이슬 <omjl5123@catholic.ac.kr>
2024-03-05 22:16:49 +0900

    [level 3] Title: 기지국 설치, Time: 1.69 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -0,0 +1,14 @@
+from math import ceil
+
+
+def solution(n: int, stations: list, w: int) -> int:
+    ww = w * 2 + 1
+    answer = 0
+    current = 1
+    for station in stations:
+        if current < station - w:
+            answer += ceil((station - w - current) / ww)
+        current = station + w + 1
+    answer += ceil((n - current + 1) / ww)
+        
+    return answer
