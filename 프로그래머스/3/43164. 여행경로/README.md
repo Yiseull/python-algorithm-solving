5034391cc9b3f7b7e9ae55274582558a1e1d6eed
박이슬 <omjl5123@catholic.ac.kr>
2024-03-11 15:55:20 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164#) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 11일 15:50:57
+2024년 03월 11일 15:55:18
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,6 +1,24 @@
 from collections import defaultdict
 
 
+def dfs(n: int, tickets_dict: dict, start: int, cnt: int) -> None:
+        if cnt == n:
+            return [start]
+        
+        for i, city in enumerate(tickets_dict[start]):
+            if city == '':
+                continue
+                
+            tickets_dict[start][i] = ''
+            result = dfs(n, tickets_dict, city, cnt + 1)
+            tickets_dict[start][i] = city
+            
+            if result != -1:
+                return [start] + result
+            
+        return -1
+    
+
 def solution(tickets: list) -> list:
     n = len(tickets)
     tickets_dict = defaultdict(list)
@@ -10,18 +28,5 @@ def solution(tickets: list) -> list:
         
     for key in tickets_dict.keys():
         tickets_dict[key].sort()
-        
-    def dfs(start: int, cnt: int) -> None:
-        if cnt == n:
-            return [start]
-        for i, city in enumerate(tickets_dict[start]):
-            if city == '':
-                continue
-            tickets_dict[start][i] = ''
-            result = dfs(city, cnt + 1)
-            tickets_dict[start][i] = city
-            if result != -1:
-                return [start] + result
-        return -1
     
-    return dfs('ICN', 0)
+    return dfs(n, tickets_dict, 'ICN', 0)
