a9b30a88db8a4d6c9263de300d9cb911a61b8aaf
박이슬 <omjl5123@catholic.ac.kr>
2024-03-18 16:47:21 +0900

    [level 3] Title: 단속카메라, Time: 2.25 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/README.md
+++ 프로그래머스/3/42884. 단속카메라/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단속카메라 - 42884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 0.84 ms
+메모리: 10.7 MB, 시간: 2.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 15:41:50
+2024년 03월 18일 16:47:19
 
 ### 문제 설명
 
--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,10 +1,23 @@
+'''
+1. 진입 시점으로 정렬한다.
+2. 차량의 진입 시점을 지날 때마다 진출 지점을 우선순위 큐에 넣는다.
+3. 다음 차량의 진입 시점이 이전에 저장해둔 가장 작은 진출 지점보다 크면 우선순위 큐를 비우고, 정답을 +1한다.
+4. 마지막 차량을 다 확인한 후 큐가 비어있지 않으면, 정답을 +1한다.
+'''
+from heapq import *
+
+
 def solution(routes: list) -> int:
-    answer = 1
-    routes.sort(key=lambda x: x[1])
-    camera = routes[0][1]
-    for start, end in routes[1:]:
-        if camera < start:
+    answer = 0
+    q = []
+    routes.sort()
+    for route in routes:
+        if q and q[0] < route[0]:
+            q = []
             answer += 1
-            camera = end
-        
+        heappush(q, route[1])
+            
+    if q:
+        answer += 1
+            
     return answer
