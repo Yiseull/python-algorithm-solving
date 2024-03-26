1ab208f301dafbca5bc5d891a1a19b8a0af9137a
박이슬 <omjl5123@catholic.ac.kr>
2024-03-26 20:14:36 +0900

    [level 2] Title: [3차] n진수 게임, Time: 4.37 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -0,0 +1,22 @@
+def change_base_n(n: int, num: int) -> str:
+    result = ''
+    while num > 0:
+        a = num % n
+        if a > 9:
+            result += chr(65 + a - 10)
+        else:
+            result += str(a)
+        num //= n
+    return result[::-1]
+
+
+def solution(n: int, t: int, m: int, p: int) -> list:
+    answer = '0'
+    last_time = m * (t - 1) + p
+    i = 1
+    while len(answer) < last_time:
+        answer += change_base_n(n, i)
+        i += 1
+        
+    answer = list(answer)
+    return ''.join(answer[p - 1:last_time:m])
