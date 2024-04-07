9596b8dba9283643d7c0d955d2ed14f93c77618b
박이슬 <omjl5123@catholic.ac.kr>
2024-04-07 19:40:47 +0900

    [level 2] Title: 순위 검색, Time: 577.62 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.py
@@ -2,24 +2,23 @@ from bisect import bisect_left
 from collections import defaultdict
 
 
-def solution(info, query):
-    answer = []
+def solution(infos: list, queries: list) -> list:
     info_dict = defaultdict(list)
+    for info in infos:
+        language, job, career, food, score = info.split()
+        for l in ('-', language):
+            for j in ('-', job):
+                for c in ('-', career):
+                    for f in ('-', food):
+                        info_dict[(l, j, c, f)].append(int(score))
     
-    for i in info:
-        data = i.split()
-        for lang in (data[0], '-'):
-            for job in (data[1], '-'):
-                for career in (data[2], '-'):
-                    for food in (data[3], '-'):
-                        info_dict[(lang, job, career, food)].append(int(data[4]))
-                        
     for key in info_dict.keys():
         info_dict[key].sort()
     
-    for q in query:
-        lang, job, career, food = q.split(' and ')
-        food, score = food.split()
-        answer.append(len(info_dict[(lang, job, career, food)]) - bisect_left(info_dict[(lang, job, career, food)], int(score)))
-    
+    answer = []
+    for query in queries:
+        language, job, career, food_score = query.split(' and ')
+        food, score = food_score.split()
+        left = bisect_left(info_dict[(language, job, career, food)], int(score))
+        answer.append(len(info_dict[(language, job, career, food)]) - left)
     return answer
